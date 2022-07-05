bc16ec98e531d9aafc5a00d024608adf5f7aa958
Inzamam <inzamam@credencesoft.in>
2022-07-05 17:40:49 +0700

    Created TopBar for the table

--- pages/index.js
+++ pages/index.js
@@ -6,28 +6,204 @@ import {AiFillThunderbolt} from 'react-icons/ai';
 import {MdOutlineArrowDropDown} from 'react-icons/md';
 import {BsFillTagFill} from 'react-icons/bs';
 import {BiSearch} from 'react-icons/bi';
-import {GoTriangleRight} from 'react-icons/go'
+import {GoTriangleRight} from 'react-icons/go';
+import {MdKeyboardArrowLeft,MdKeyboardArrowRight} from 'react-icons/md';
+import {AiOutlineDoubleRight,AiOutlineDoubleLeft} from 'react-icons/ai';
+import {GrRotateLeft} from 'react-icons/gr'
 
 export default function Home() {
+  console.log(styles);
   return (
     <div className={styles.container}>
       <div className={styles.table}>
-        <div className={styles.buttonGroup}>
+      <div className={styles.topBar}>
+        <div className={styles.topBarButtons}>
+         <div className={styles.buttonsWrapper}>
+          <button className={styles.bulkUpdateBtn}>Bulk Update</button>
+          <span><GrRotateLeft/>Reset</span>
+          <button className={styles.saveBtn}>Save</button>
+         </div>
+        </div>
+        <Row className={styles.dateSection}>
+          <Col className={styles.dateSelector}>
+            <span><GrRotateLeft size={15} style={{marginRight: '10px'}}/><AiOutlineDoubleLeft size={15} style={{marginRight: '10px'}}/><MdKeyboardArrowLeft/>Jul 5, 2022<MdKeyboardArrowRight /><AiOutlineDoubleRight size={15} style={{marginLeft: '10px'}} /></span>
+          </Col>
+          <Col className={styles.dates}>
+            <Row className={styles.dateCards}>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+              <Col className={styles.dateCard}>
+              <span>
+                Mon
+              </span>
+              <span className={styles.boldDateText}>
+                03
+              </span>
+              <span>
+                JUL
+              </span>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+      <div className={styles.buttonGroup}>
           <Row>
             <Col className={styles.buttons}>
-              <button className={styles.button}>All Rates & Availablity<span><MdOutlineArrowDropDown /></span></button>
-              <button className={styles.button}><FaBed style={{marginRight: '8px',marginBottom: '4px'}} size={15} />All Room Types<MdOutlineArrowDropDown size={18} style={{marginLeft: '2px',marginBottom: '2px'}}  /></button>
-              <button className={styles.button}><BsFillTagFill style={{marginRight: '8px',marginBottom: '2px'}}/>All Rates Plans<MdOutlineArrowDropDown size={18}  style={{marginLeft: '2px',marginBottom: '2px'}} /></button>
+              <button className={`${styles.buttonHover} ${styles.button} `}>All Rates & Availablity<span><MdOutlineArrowDropDown size={22} style={{marginBottom: '2px'}}/></span></button>
+              <button className={styles.button}><FaBed style={{marginRight: '8px',marginBottom: '2px'}} size={15} />All Room Types<MdOutlineArrowDropDown size={22} style={{marginLeft: '2px',marginBottom: '2px'}}  /></button>
+              <button className={styles.button}><BsFillTagFill style={{marginRight: '8px',marginBottom: '2px'}}/>All Rates Plans<MdOutlineArrowDropDown size={22}  style={{marginLeft: '2px',marginBottom: '4px'}} /></button>
               <div className={styles.inputItem}><span><BiSearch size={15} style={{marginBottom: '1px'}}/></span><input placeholder='Search room Rates'/></div>
               <span>Clear all filters</span>
             </Col>
             <Col className={styles.rightlinkText}>
               <div className={styles.linkText}>
-                <GoTriangleRight /><span>Quick Tour - Inventory Grid</span>
+                <GoTriangleRight size={20} style={{marginBottom : '3px'}} /><span>Quick Tour - Inventory Grid</span>
               </div>
             </Col>
           </Row>
-        </div>
+      </div>
       <div className={styles.item}>
         <Row className={styles.heading}>
           <Col className={styles.Icon}>
