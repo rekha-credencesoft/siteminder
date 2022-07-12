0565d2c7bc9e701b3b150b5c93f42a08fcf6592d
zuhair-credencesoft <zuhair@credencesoft.in>
2022-07-12 10:19:43 +0530

    Initial Commit

--- pages/experiment.js
+++ pages/experiment.js
@@ -6,14 +6,27 @@ import { MdArrowLeft, MdArrowRight } from "react-icons/md";
 const Experiment = () => {
   return (
     <div className={styles.container}>
-      <Row>
+      <Row className={styles.row}>
         <Col className={styles.leftArrow}>
           <MdArrowLeft />
         </Col>
         <Col className={styles.columnDate}>
-          <span>Mon</span>
-          <span>11</span>
-          <span>Jul</span>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              backgroundColor: "white",
+              color: "#9acc54",
+              padding: "5px 20px",
+              borderRadius: "5px",
+              margin: "8px 0px",
+            }}
+          >
+            <span>Mon</span>
+            <span>11</span>
+            <span>Jul</span>
+          </div>
         </Col>
         <Col className={styles.columnDate}>
           <span>Mon</span>
