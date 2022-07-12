6067c4db019e741004a54b7a3e5d2b73c259d694
zuhair-credencesoft <zuhair@credencesoft.in>
2022-07-12 09:13:27 +0530

    Created Top Bar

--- pages/experiment.js
+++ pages/experiment.js
@@ -1,8 +1,71 @@
 import React from "react";
 import styles from "../styles/experiment.module.css";
+import { Row, Col } from "react-bootstrap";
+import { MdArrowLeft, MdArrowRight } from "react-icons/md";
 
 const Experiment = () => {
-  return <div>Experiment</div>;
+  return (
+    <div className={styles.container}>
+      <Row>
+        <Col className={styles.leftArrow}>
+          <MdArrowLeft />
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.columnDate}>
+          <span>Mon</span>
+          <span>11</span>
+          <span>Jul</span>
+        </Col>
+        <Col className={styles.rightArrow}>
+          <MdArrowRight />
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Experiment;
