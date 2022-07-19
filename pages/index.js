7432fc2c2d84081bc6c33feb58cd6131b7c9ede8
rekha-credencesoft <rekha@credencesoft.in>
2022-07-19 23:08:11 +0530

    Added siteminder link

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Index() {
   return (
-    <div><Link href={'http://localhost:3000/propertyId/539'}>Link here</Link></div>
+    <div><Link href={'https://siteminder-omega.vercel.app/propertyId/575'}>Link here</Link></div>
   )
 }
 
